e987ba23b937cdf435c8f8622eff542db653134e
Pixelmania <84938061+Malishade@users.noreply.github.com>
2023-01-22 20:22:50 +0100

    slight refactor on ModelController

--- Assets/Scripts/ModelController.cs
+++ Assets/Scripts/ModelController.cs
@@ -5,6 +5,9 @@ public class ModelController : MonoBehaviour
     [SerializeField] private float _mouseSpeed = 10f; 
     [SerializeField] private ModelViewer _modelViewer;
 
+    private Vector2 _mouseMoveAxis => new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+    private float _mouseScrollAxis => Input.GetAxis("Mouse ScrollWheel");
+
     void Update()
     {
         if (_modelViewer.CurrentModel == null)
@@ -12,25 +15,25 @@ public class ModelController : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            float rotX = Input.GetAxis("Mouse X") * _mouseSpeed;
-            float rotY = Input.GetAxis("Mouse Y") * _mouseSpeed;
+            var mouseMoveAxis = _mouseMoveAxis * _mouseSpeed;
 
-            _modelViewer.PivotPoint.transform.Rotate(0f, -rotX, 0f, Space.World);
-            _modelViewer.PivotPoint.transform.Rotate(rotY, 0f, 0f, Space.Self);
+            _modelViewer.PivotPoint.transform.Rotate(0f, -mouseMoveAxis.x, 0f, Space.World);
+            _modelViewer.PivotPoint.transform.Rotate(mouseMoveAxis.y, 0f, 0f, Space.Self);
         }
 
         if (Input.GetMouseButton(1))
         {
-            float rotX = Input.GetAxis("Mouse X") * _mouseSpeed / 20;
-            float rotY = Input.GetAxis("Mouse Y") * _mouseSpeed / 20;
+            var mouseMoveAxis = _mouseMoveAxis * _mouseSpeed / 20;
 
-            _modelViewer.PivotPoint.transform.Translate(0f, rotY, 0f, Space.World);
-            _modelViewer.PivotPoint.transform.Translate(rotX, 0f, 0f, Space.World);
+            _modelViewer.PivotPoint.transform.Translate(0f, mouseMoveAxis.y, 0f, Space.World);
+            _modelViewer.PivotPoint.transform.Translate(mouseMoveAxis.x, 0f, 0f, Space.World);
         }
 
         if (Input.GetAxis("Mouse ScrollWheel") != 0f)
         {
-            _modelViewer.PivotPoint.transform.Translate(0, 0, -Input.GetAxis("Mouse ScrollWheel") * _mouseSpeed, Space.World);
+            var mouseScrollAxis = _mouseScrollAxis * _mouseSpeed;
+
+            _modelViewer.PivotPoint.transform.Translate(0, 0, -mouseScrollAxis, Space.World);
         }
     }
 }
