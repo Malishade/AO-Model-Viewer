002c6c23d41b1d696d66a90504aa0ef5c4800b18
Pixelmania <84938061+Malishade@users.noreply.github.com>
2023-01-23 02:09:08 +0100

    PivotController model manipulation is now time independant and smoother

--- Assets/Scripts/PivotController.cs
+++ Assets/Scripts/PivotController.cs
@@ -1,53 +1,82 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PivotController : MonoBehaviour
 {
     [SerializeField] private float _sensitivity = 10f;
     [SerializeField] private float _rotSmoothness = 0.1f;
-    [SerializeField] private float _moveSmoothness = 0.1f;
+    [SerializeField] private float _xySmoothness = 0.1f;
+    [SerializeField] private float _zSmoothness = 0.1f;
 
-    private Vector2 _mouseMoveAxis => new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * _sensitivity * 100 * Time.deltaTime;
-    
-    private float _mouseScrollAxis => Input.GetAxis("Mouse ScrollWheel");
- 
+    private const float _xRotClamp = 90f;
+    private const float _nearZero = 0.001f;
+
     private Vector2 _localRotation = Vector2.zero;
-    private Vector2 _localPosition = Vector2.zero;
+    private Vector3 _localPosition = Vector2.zero;
+    private Vector3 _mouseInput => new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse ScrollWheel")) * _deltaSensitivity;
 
-    private float _xRotClamp = 90f;
+    private float deltaTimeConst => 100 * Time.deltaTime;
+    private float _deltaSensitivity => _sensitivity * deltaTimeConst;
+    private float _deltaRotSmoothness => _rotSmoothness * deltaTimeConst;
+    private float _deltaXySmoothness => _xySmoothness* deltaTimeConst;
+    private float _deltaZSmoothness => _zSmoothness * deltaTimeConst;
 
     void Update()
     {
+        LeftClickInput();
+        RightClickInput();
+        ScrollWheelInput();
+    }
+
+    private void LeftClickInput()
+    {
         if (Input.GetMouseButton(0))
         {
-            _localRotation.x -= _mouseMoveAxis.y;
+            _localRotation.x -= _mouseInput.y;
             _localRotation.x = Mathf.Clamp(_localRotation.x, -_xRotClamp, _xRotClamp);
 
-            _localRotation.y += _mouseMoveAxis.x;
+            _localRotation.y += _mouseInput.x;
+        }
 
+        if (Vector2.Distance(_localRotation, transform.localEulerAngles) != 0)
+        {
             float currentX = transform.localEulerAngles.x;
             float currentY = transform.localEulerAngles.y;
 
-            currentX = Mathf.LerpAngle(currentX, _localRotation.x, _rotSmoothness * 100 * Time.deltaTime);
-            currentY = Mathf.LerpAngle(currentY, _localRotation.y, _rotSmoothness * 100 * Time.deltaTime);
+            currentX = Mathf.LerpAngle(currentX, _localRotation.x, _deltaRotSmoothness);
+            currentY = Mathf.LerpAngle(currentY, _localRotation.y, _deltaRotSmoothness);
 
             transform.localRotation = Quaternion.Euler(currentX, currentY, 0f);
         }
+    }
 
-
+    private void RightClickInput()
+    {
         if (Input.GetMouseButton(1))
         {
-            var mouseMoveAxis = _mouseMoveAxis / 10;
+            var mouseMoveAxis = new Vector3(_mouseInput.x, _mouseInput.y, 0) / 10;
 
             _localPosition += mouseMoveAxis;
-
-            transform.position = Vector3.Lerp(transform.position, new Vector3(_localPosition.x, _localPosition.y, transform.position.z), _moveSmoothness * 100 * Time.deltaTime);
         }
 
+        if (Vector2.Distance(new Vector2(_localPosition.x, _localPosition.y), new Vector2(transform.position.x, transform.position.y)) > _nearZero)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(_localPosition.x, _localPosition.y, transform.position.z), _deltaXySmoothness);
+        }
+    }
+
+    private void ScrollWheelInput()
+    {
         if (Input.GetAxis("Mouse ScrollWheel") != 0f)
         {
-            var mouseScrollAxis = _mouseScrollAxis * 5f;
+            var mouseScrollAxis = new Vector3(0, 0, _mouseInput.z) * 2f;
 
-            transform.Translate(Vector3.forward * -mouseScrollAxis, Space.World);
+            _localPosition -= mouseScrollAxis;
+        }
+
+        if (Mathf.Abs(_localPosition.z - transform.position.z) > _nearZero)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y, _localPosition.z), _deltaZSmoothness);
         }
     }
 
